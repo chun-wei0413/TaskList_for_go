294c7468ea0c0fc06856256667c4d23e5e466df6
李俊威 <113598009@cc.ntut.edu.tw>
2025-03-23 22:54:16 +0800

    [Feature Addition] (1)Added UT for tasks.go (2) Added tasks.go and move it to entity package.

--- tasklist/entity/tasks.go
+++ tasklist/entity/tasks.go
@@ -1,22 +1,20 @@
-package tasklist
-
-import "TaskList_for_go/tasklist/entity"
+package entity
 
 // Entry 結構體用於儲存鍵值對
 type Entry struct {
 	Key   string
-	Value []entity.Task
+	Value []Task
 }
 
 // Tasks 結構體
 type Tasks struct {
-	tasks map[string][]entity.Task
+	tasks map[string][]Task
 }
 
 // NewTasks 構造函數，初始化 Tasks
 func NewTasks() *Tasks {
 	return &Tasks{
-		tasks: make(map[string][]entity.Task),
+		tasks: make(map[string][]Task),
 	}
 }
 
@@ -30,11 +28,11 @@ func (t *Tasks) EntrySet() []Entry {
 }
 
 // Put 方法，將任務列表與專案名稱關聯
-func (t *Tasks) Put(name string, tasks []entity.Task) {
+func (t *Tasks) Put(name string, tasks []Task) {
 	t.tasks[name] = tasks
 }
 
 // Get 方法，根據專案名稱返回任務列表
-func (t *Tasks) Get(project string) []entity.Task {
+func (t *Tasks) Get(project string) []Task {
 	return t.tasks[project]
 }
